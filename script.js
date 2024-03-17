cee0c6114524b4770de1403d4d1a7d51b2094f6d
Kidokatto <gremory220@hotmail.com>
2024-03-17 13:57:02 -0500

    Script menu hamburguesa y html del login

--- script.js
+++ script.js
@@ -1,15 +1,8 @@
-let loadMoreBtn1 = document.querySelector('#load-more-1');
-let currentItem1 = 4;
-
-loadMoreBtn1.onclick = () => {
-    let boxes = document.querySelectorAll('.box-container-1 .box-1');
-    for (let i = currentItem1; i < currentItem1 + 4; i++) {
-        if (i < boxes.length) {
-            boxes[i].style.display = 'inline-block';
-        }
-    }
-    currentItem1 += 4;
-    if (currentItem1 >= boxes.length) {
-        loadMoreBtn1.disabled = true; // Desactivar el botón en lugar de ocultarlo
-    }
-};
+document.querySelector('.menu-hamburguesa').addEventListener('click', function() {
+  if (this.classList.contains('rotada')) {
+    this.style.transform = 'rotate(0deg)'; // Si el botón está rotado, lo volvemos a la posición original
+  } else {
+    this.style.transform = 'rotate(90deg)'; // Si el botón no está rotado, lo rotamos
+  }
+  this.classList.toggle('rotada'); // Alternamos la clase rotada para mantener el estado
+});
