938d305ad7d87490ea2137d6902b173e44043782
Kidokatto <gremory220@hotmail.com>
2024-03-21 00:03:58 -0500

    se agrego un modal que despliega informacion del juego y video

--- script.js
+++ script.js
@@ -6,3 +6,13 @@ document.querySelector('.menu-hamburguesa').addEventListener('click', function()
   }
   this.classList.toggle('rotada'); // Alternamos la clase rotada para mantener el estado
 });
+
+// Función para abrir el modal
+function abrirModal(id) {
+  // Obtener el checkbox que controla el modal
+  var checkbox = document.getElementById(id);
+  // Cambiar su estado de marcado/desmarcado
+  checkbox.checked = !checkbox.checked;
+  
+}
+
